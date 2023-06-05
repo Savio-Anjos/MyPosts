c4bb1afc778a3cd152ec4e1c107c3d17c9a827d9
savio <savioanjos2020@gmail.com>
2023-06-04 21:18:49 -0300

    :sparkles: feat: Recuperando os dados retornados da API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { UserService } from './shared/user.service';
+import { User } from 'src/app/User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   isCollapsed = false;
+  user: User = {
+    id: '',
+    name: '',
+    email: '',
+  };
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.fetchUser();
+  }
+
+  fetchUser() {
+    this.userService.fetchUser().subscribe((user) => {
+      this.user = user;
+      console.log(this.user);
+    });
+  }
 }
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NzCardModule } from 'ng-zorro-antd/card';
 import { Post } from 'src/app/Post';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { ListService } from 'src/app/services/list.service';
 
 @Component({
