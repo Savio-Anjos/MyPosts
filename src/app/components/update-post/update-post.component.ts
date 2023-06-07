a68d41f004b567799d1ec979de78198ac1026523
savio <savioanjos2020@gmail.com>
2023-06-07 11:45:03 -0300

    :sparkles: feat: Criando validações do formulário UpdatePost

--- src/app/components/update-post/update-post.component.ts
+++ src/app/components/update-post/update-post.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-post',
   templateUrl: './update-post.component.html',
-  styleUrls: ['./update-post.component.scss']
+  styleUrls: ['./update-post.component.scss'],
 })
 export class UpdatePostComponent {
+  updateForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.updateForm = new FormGroup({
+      post: new FormControl('', [Validators.required]),
+      body: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get post() {
+    return this.updateForm.get('post')!;
+  }
+
+  get body() {
+    return this.updateForm.get('body')!;
+  }
+
+  submit() {
+    if (this.updateForm.invalid) {
+      return;
+    }
+
+    console.log('Formulário enviado');
+  }
 }
