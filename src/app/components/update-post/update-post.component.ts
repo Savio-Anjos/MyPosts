96d4ea950eaf57d3395034e4a8c80539e71b5ee5
savio <savioanjos2020@gmail.com>
2023-06-07 12:45:09 -0300

    :sparkles: feat: Listando todas as postagens do usuário no select

--- src/app/components/update-post/update-post.component.ts
+++ src/app/components/update-post/update-post.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from 'src/app/shared/user.service';
+import { UserStateService } from 'src/app/shared/user-state.service';
+import { ListPostUserService } from 'src/app/services/list-post-user.service';
+import { User } from 'src/app/User';
+import { Post } from 'src/app/Post';
 
 @Component({
   selector: 'app-update-post',
@@ -8,12 +13,21 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class UpdatePostComponent {
   updateForm!: FormGroup;
+  user?: User | null;
+  postsUser: Post[] = [];
+
+  constructor(
+    private userService: UserService,
+    private userStateService: UserStateService,
+    private listPostUserService: ListPostUserService
+  ) {}
 
   ngOnInit(): void {
     this.updateForm = new FormGroup({
       post: new FormControl('', [Validators.required]),
       body: new FormControl('', [Validators.required]),
     });
+    this.fetchPostsUser(Number(this.currentUser?.id));
   }
 
   get post() {
@@ -22,6 +36,17 @@ export class UpdatePostComponent {
 
   get body() {
     return this.updateForm.get('body')!;
+  }
+
+  get currentUser(): User | null {
+    this.user = this.userStateService.getCurrentUser();
+    return this.user;
+  }
+
+  fetchPostsUser(id: number) {
+    this.listPostUserService.fetchPostsUser(id).subscribe((posts) => {
+      this.postsUser = posts;
+    });
   }
 
   submit() {
