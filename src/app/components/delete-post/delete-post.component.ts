987777e9be35978b05db91f742931fc91824599b
savio <savioanjos2020@gmail.com>
2023-06-08 21:51:33 -0300

    :sparkles: feat: Listando postagens do usuário no select de DeletePost

--- src/app/components/delete-post/delete-post.component.ts
+++ src/app/components/delete-post/delete-post.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { UserStateService } from 'src/app/shared/user-state.service';
+import { ListPostUserService } from 'src/app/services/list-post-user.service';
+
+import { User } from 'src/app/User';
+import { Post } from 'src/app/Post';
 @Component({
   selector: 'app-delete-post',
   templateUrl: './delete-post.component.html',
@@ -10,21 +16,39 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class DeletePostComponent implements OnInit {
   deleteForm!: FormGroup;
-
+  user?: User | null;
+  postsUser: Post[] = [];
   buttonNewPost: boolean = false;
   textButton: string = 'deletar publicação';
 
   alertSucess: boolean = false;
   alertError: boolean = false;
 
+  constructor(
+    private userStateService: UserStateService,
+    private listPostUserService: ListPostUserService
+  ) {}
+
   ngOnInit(): void {
     this.deleteForm = new FormGroup({
       post: new FormControl('', Validators.required),
     });
+    this.fetchPostsUser(Number(this.currentUser?.id));
   }
 
   get post() {
     return this.deleteForm.get('post');
+  }
+
+  get currentUser(): User | null {
+    this.user = this.userStateService.getCurrentUser();
+    return this.user;
+  }
+
+  fetchPostsUser(id: number) {
+    this.listPostUserService.fetchPostsUser(id).subscribe((posts) => {
+      this.postsUser = posts;
+    });
   }
 
   submit() {
