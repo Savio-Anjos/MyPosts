37c84a945265bbb7188d5953c405f99284713385
Savio-Anjos <savioanjos2020@gmail.com>
2023-08-05 20:33:12 -0300

    :white_check_mark: test: Configurando testes e2e para página de listagem

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,22 @@
 describe('MyPost Test', () => {
   it('Full Test', () => {
     cy.visit('/');
+
+    //Página de listagem
+    cy.get('a[routerlink="/welcome/list"]')
+      .contains('Listar todas as publicações')
+      .click();
+
+    cy.get('input[class="ng-untouched ng-pristine ng-valid"').invoke('val', 10);
+
+    cy.wait(1000);
+
+    cy.get('button[class="ant-btn ant-btn-primary"]').click();
+
+    cy.get('a[routerlink="/welcome"]').contains('Welcome').click();
+
+    cy.get('a[routerlink="/welcome/userPosts"]')
+      .contains('Listar publicações do usuário conectado')
+      .click();
   });
 });
