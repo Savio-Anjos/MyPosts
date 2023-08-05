595291f22964887639ae658db8055b44a2dced8b
Savio-Anjos <savioanjos2020@gmail.com>
2023-08-05 20:36:57 -0300

    :white_check_mark: test: Configurando testes e2e para página de criação de posts

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -30,5 +30,41 @@ describe('MyPost Test', () => {
     cy.get('a[routerlink="/welcome"]').contains('Welcome').click();
 
     cy.get('span[class="ng-star-inserted"]').contains('Dashboard').click();
+
+    //Página de criação de Posts
+
+    cy.get('a[routerlink="/welcome/createPosts"]')
+      .contains('Criar Publicação')
+      .click();
+
+    cy.get('input[id="title"]')
+      .filter('.ng-untouched')
+      .type('Minha publicação');
+
+    cy.get(
+      'textarea[class="ng-untouched ng-pristine ng-invalid ng-star-inserted"]'
+    ).type('Conteúdo da minha publicação');
+
+    cy.get('button[class="ant-btn ant-btn-primary ng-star-inserted"]').click();
+
+    cy.get('span[class="ng-star-inserted"]').contains('Ver Publicação').click();
+
+    cy.get('span[class="ng-star-inserted"]').contains('Cancel').click();
+
+    cy.get('span[class="ng-star-inserted"]').contains('Ver Publicação').click();
+
+    cy.get('span[class="ng-star-inserted"]').contains('OK').click();
+
+    cy.get('span[class="ng-star-inserted"]')
+      .contains('Nova Publicação')
+      .parents(
+        'button[class="ant-btn button-new-post ant-btn-primary ng-star-inserted"]'
+      )
+      .click();
+    cy.get('span[class="ng-star-inserted"]').contains('Dashboard').click();
+
+    cy.get('a[routerlink="/welcome"]').contains('Welcome').click();
+
+    cy.get('a[routerlink="/welcome/updatePosts"]');
   });
 });
